04b8b4bb79466d36b4e3ec45c5a780d525000e65
Naveen Kumar Reddy Pemmana <mr.naveen8@gmail.com>
2022-10-09 16:50:33 +0530

    isort and lint

--- blogs/models.py
+++ blogs/models.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,12 +12,16 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
 
     class Meta:
         ordering = ["-publish"]
@@ -25,4 +29,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
