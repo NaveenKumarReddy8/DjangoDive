49d138178609b5cb50ea7dbc0bbccc959bb9fa17
Naveen Kumar Reddy Pemmana <mr.naveen8@gmail.com>
2022-11-01 18:32:35 +0530

    updated packages and reverted the strucutre

--- blogs/models.py
+++ blogs/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 
@@ -29,3 +30,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(viewname="blogs:post_details", args=(self.id, ))
