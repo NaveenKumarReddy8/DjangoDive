37db39e97ef0872a80b72dd3c7239d2bd17e9b70
Naveen Kumar Reddy Pemmana <mr.naveen8@gmail.com>
2022-11-07 09:43:59 +0530

    updated for email

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from blogs.views import post_detail, post_list
+from blogs.views import PostListView, post_detail
 
 app_name = "blogs"
 urlpatterns = [
-    path("", post_list, name="post_list"),
-    path("<int:year>/<int:month>/<int:day>/<slug:post>", post_detail, name="post_details"),
+    # path("", post_list, name="post_list"),
+    path("", PostListView.as_view(), name="post_list"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:post>", post_detail, name="post_details"
+    ),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,12 +1,24 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from typing import Optional
+
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpRequest
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.views.generic import ListView
 
 from blogs.models import Post
+from blogs.forms import EmailPostForm
 
 # Create your views here.
 
 
+class PostListView(ListView):
+    model = Post
+    template_name = "blogs/post/list.xhtml"
+    context_object_name: Optional[str] = "posts"
+    paginate_by: int = 3
+
+
+# This view function is replaced by class based View PostListView.
 def post_list(request: HttpRequest):
     post_list = get_list_or_404(Post)
     paginator = Paginator(post_list, 1)
@@ -29,3 +41,13 @@ def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str
     return render(
         request=request, template_name="blogs/post/detail.xhtml", context={"post": post}
     )
+
+def post_share(request: HttpRequest, post_id: int):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form: EmailPostForm = EmailPostForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request=request, template_name="blogs/post/share.xhtml", context={"post": post, "form": form})
