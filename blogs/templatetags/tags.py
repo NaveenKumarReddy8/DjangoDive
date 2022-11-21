f3d7664be31c441f3cd19a2663aa3e27e1eea502
naveen <mr.naveen8@gmail.com>
2022-11-22 02:18:17 +0530

    Implemented sitemaps

--- blogs/templatetags/tags.py
+++ blogs/templatetags/tags.py
@@ -1,4 +1,7 @@
+from django.db.models import Count
 from django.template import Library
+from django.utils.safestring import mark_safe
+from markdown import markdown
 
 from blogs.models import Post
 
@@ -13,3 +16,17 @@ def total_posts():
 @register.inclusion_tag(filename="blogs/post/latest_posts.xhtml")
 def latest_posts(count: int = 5):
     return {"latest_posts": Post.objects.filter().order_by("-publish")[:count]}
+
+
+@register.simple_tag
+def get_most_commented_posts(count: int = 5):
+    return (
+        Post.objects.all()
+        .annotate(total_comments=Count("comments"))
+        .order_by("-total_comments")[:count]
+    )
+
+
+@register.filter(name="markdown")
+def markdown_format(text: str):
+    return mark_safe(markdown(text))
