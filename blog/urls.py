75ce08e7c2d19523da7c6b6938f9d3d3544de2d5
Naveen Kumar Reddy Pemmana <mr.naveen8@gmail.com>
2023-12-30 19:26:49 +0530

    completed urls and created base templates

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from blog.views import post_detail, post_list
 
-url_patterns = [
+app_name = "blog"
+
+urlpatterns = [
     path("", post_list, name="post_list"),
     path("<int:id>/", post_detail, name="post_detail"),
 ]
