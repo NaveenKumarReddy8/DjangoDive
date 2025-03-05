e90d35a0da0d5d04ab4e160deb34ea0c9dd8f513
naveenkumarreddypemmana <naveenkumarreddypemmana@gmail.com>
2025-03-05 10:42:57 +0000

    ruff linting

--- djangodive/wsgi.py
+++ djangodive/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangodive.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangodive.settings")
 
 application = get_wsgi_application()
