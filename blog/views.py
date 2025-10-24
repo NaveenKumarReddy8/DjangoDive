a5a4f7141e0b230f34877f10b1c5f40a398e0f07
naveenkumarreddypemmana <naveenkumarreddypemmana@gmail.com>
2025-10-24 06:56:17 +0000

    started views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import Http404, HttpRequest, HttpResponse
+
+from blog.models import Post
+
 
 # Create your views here.
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    posts = Post.published.all()
+    return render(
+        request=request, template_name="blog/post/list.html", context={"posts": posts}
+    )
