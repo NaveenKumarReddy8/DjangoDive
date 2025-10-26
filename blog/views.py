ef52401a2563623eec69f4fe117af4f20a85f446
naveenkumarreddypemmana <naveenkumarreddypemmana@gmail.com>
2025-10-26 02:50:01 +0000

    started working on templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpRequest, HttpResponse
 
 from blog.models import Post
@@ -11,4 +11,13 @@ def post_list(request: HttpRequest) -> HttpResponse:
     posts = Post.published.all()
     return render(
         request=request, template_name="blog/post/list.html", context={"posts": posts}
+    )
+
+
+def post_detail(request: HttpRequest, id: int) -> HttpResponse:
+    post = get_object_or_404(klass=Post, id=id, status=Post.Status.PUBLISHED)
+    return render(
+        request=request,
+        template_name="blog/post/detail.html",
+        context={"post": post},
     )
