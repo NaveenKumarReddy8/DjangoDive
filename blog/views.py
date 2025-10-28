28a6768fd75629773f1790102971b7a3ef13bb0a
naveenkumarreddypemmana <naveenkumarreddypemmana@gmail.com>
2025-10-28 01:46:53 +0000

    Working on day 5

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpRequest, HttpResponse
 from django.core.paginator import Paginator
+from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from blog.models import Post
+from blog.forms import EmailPostForm
 
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_list(request: HttpRequest) -> HttpResponse:
@@ -33,4 +43,28 @@ def post_detail(
         request=request,
         template_name="blog/post/detail.html",
         context={"post": post},
+    )
+
+
+def post_share(request: HttpRequest, post_id: int) -> HttpResponse:
+    post = get_object_or_404(klass=Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            send_mail(
+                subject=f"{cd['name']} recommends you read {post.title}",
+                message=f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}",
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request=request,
+        template_name="blog/post/share.html",
+        context={"post": post, "form": form, "sent": sent},
     )
