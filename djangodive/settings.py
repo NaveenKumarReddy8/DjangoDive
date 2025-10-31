d19cab64ec7b3dc09a6af6b625e6d27804eb4962
naveenkumarreddypemmana <naveenkumarreddypemmana@gmail.com>
2025-10-31 13:46:30 +0000

    postgres docker setup

--- djangodive/settings.py
+++ djangodive/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = "djangodive.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
