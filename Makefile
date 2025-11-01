3c211898e55585624c30ee10a48c7ff38c3038c0
naveenkumarreddypemmana <naveenkumarreddypemmana@gmail.com>
2025-11-01 11:05:13 +0000

    working on day 8 and day 9

--- Makefile
+++ Makefile
@@ -2,6 +2,3 @@ lint:
 	uv run ruff format .
 	uv run djlint --reformat .
 	uv run djlint --lint .
-
-server:
-	uv run python manage.py runserver
---
+++ project_bookmarks/account/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+from account.views import user_login
+
+
+urlpatterns = [path("login/", user_login, name="login")]
