61fcedb51d60cf42e0cb87528b254454d9043efb
naveenkumarreddypemmana <naveenkumarreddypemmana@gmail.com>
2025-11-02 15:48:45 +0000

    Day 8 completed

--- project_bookmarks/account/urls.py
+++ project_bookmarks/account/urls.py
@@ -1,11 +1,43 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
 from account.views import user_login, dashboard
 
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-    path("", dashboard, name="dashboard")
+    path("", dashboard, name="dashboard"),
+    path("", include("django.contrib.auth.urls")),
+    # path("login/", LoginView.as_view(), name="login"),
+    # path("logout/", LogoutView.as_view(), name="logout"),
+    # path("password-change/", PasswordChangeView.as_view(), name="password_change"),
+    # path(
+    #     "password-change/done/",
+    #     PasswordChangeDoneView.as_view(),
+    #     name="password_change_done",
+    # ),
+    # path("password-reset/", PasswordResetView.as_view(), name="password_reset"),
+    # path(
+    #     "password-reset/done/",
+    #     PasswordResetDoneView.as_view(),
+    #     name="password_reset_done",
+    # ),
+    # path(
+    #     "password-reset/<uidb64>/<token>/",
+    #     PasswordResetConfirmView.as_view(),
+    #     name="password_reset_confirm",
+    # ),
+    # path(
+    #     "password-reset/complete/",
+    #     PasswordResetCompleteView.as_view(),
+    #     name="password_reset_complete",
+    # ),
 ]
